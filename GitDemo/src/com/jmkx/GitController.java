4485c30c7333364357de35fa903cc91ba05f7cc1
qt372669021 <372669021@qq.com>
2019-07-10 17:35:41 +0800

    把本地库存添加到远程

---
+++ GitDemo/src/com/jmkx/GitController.java
@@ -0,0 +1,13 @@
+package com.jmkx;
+
+public class GitController {
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+    }
+}
